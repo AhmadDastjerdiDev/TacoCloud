7cbc497a5eae6f94a1ddcc0729e8a546266780e4
Ahmad <ahmaddastjerdi.eng@gmail.com>
2026-01-20 19:10:42 +0000

    Add validation rules for inputs of design and order pages

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,8 @@
 package tacos.web;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionstatus){
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionstatus){
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionstatus.setComplete();
         return "redirect:/";
