5d6e8df0c3a4cf2600a1304515f6d8b263cc3ac8
Ahmad <ahmaddastjerdi.eng@gmail.com>
2026-01-22 21:57:54 +0000

    Pre loading the database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,15 +20,15 @@ create table if not exists Taco(
 );
 
 create table if not exists Ingredient_Ref(
+    ingredient varchar(4) not null,
     taco bigint not null,
-    taco_key bigint not null,
-    Ingredient varchar(4) not null
+    taco_key bigint not null
 );
 
 create table if not exists Ingredient(
-    id varcher(4) not null,
+    id varchar(4) not null,
     name varchar(25) not null,
-    type varcher(10) not null
+    type varchar(10) not null
 );
 
 alter table Taco
