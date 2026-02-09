5fac1fa241a796a74b0b27aec93e87621cf77806
Ahmad <ahmaddastjerdi.eng@gmail.com>
2026-02-09 20:23:50 +0000

    add PUT and PATCH endpoint to update taco orders

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import tacos.Taco;
+import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 import tacos.data.TacoRepository;
 
 import java.util.Optional;
@@ -17,9 +20,11 @@ import java.util.Optional;
 @CrossOrigin(origins = {"http://tacocloud:8080", "http://tacocloud.com"})
 public class TacoController {
     private TacoRepository tacoRepo;
+    private OrderRepository orderRepo;
 
-    public TacoController(TacoRepository tacoRepo){
+    public TacoController(TacoRepository tacoRepo, OrderRepository orderRepo){
         this.tacoRepo = tacoRepo;
+        this.orderRepo = orderRepo;
     }
 
     @GetMapping(params = "recent")
@@ -42,5 +47,43 @@ public class TacoController {
     @ResponseStatus(HttpStatus.CREATED)
     public Taco postTaco(@RequestBody Taco taco){
         return tacoRepo.save(taco);
+    }
+
+    @PutMapping(path = "/{orderId}", consumes = "application/json")
+    public TacoOrder putOrder(@PathVariable("orderId") Long orderId,
+                              @RequestBody TacoOrder order){
+        order.setId(orderId);
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(path = "/{orderId}",consumes="application/json")
+    public TacoOrder patchOrder(@PathVariable("orderId") Long orderId,
+                                @RequestBody TacoOrder patch) {
+        TacoOrder order = orderRepo.findById(orderId).get();
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepo.save(order);
     }
 }
